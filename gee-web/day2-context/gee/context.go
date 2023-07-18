f672ee4283417f377e6d74af0d3d3eaf45882c4a
chenxu <chenxu@shanhai.team>
2023-07-18 15:44:48 +0800

    test:     http-context

--- gee-web/day2-context/gee/context.go
+++ gee-web/day2-context/gee/context.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -22,13 +23,13 @@ func newContext(w http.ResponseWriter, r *http.Request) *Context {
 	}
 }
 
-//	func (c *Context) PostForm(key string) string {
-//		return c.Req.FormValue(key)
-//	}
-//
-//	func (c *Context) Query(key string) string {
-//		return c.Req.URL.Query().Get(key)
-//	}
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
 func (c *Context) Status(code int) {
 	c.StatusCode = code
 	c.Write.WriteHeader(code)
@@ -44,23 +45,24 @@ func (c *Context) String(code int, format string, values ...interface{}) {
 	c.Write.Write([]byte(fmt.Sprintf(format, values)))
 }
 
-//// JSON /** obj的作用？
-//func (c *Context) JSON(code int, obj interface{}) {
-//	c.SetHeader("Content-Type", "application/json")
-//	c.Status(code)
-//	encoder := json.NewEncoder(c.Write)
-//	if err := encoder.Encode(obj); err != nil {
-//		http.Error(c.Write, err.Error(), 500)
-//	}
-//}
-//
-//func (c *Context) Data(code int, data []byte) {
-//	c.Status(code)
-//	c.Write.Write(data)
-//}
-//
-//func (c *Context) HTML(code int, html string) {
-//	c.SetHeader("Context-Type", "text/html")
-//	c.Status(code)
-//	c.Write.Write([]byte(html))
-//}
+// JSON /** obj的作用？
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Write)
+	// Encode将v的JSON编码写入流
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Write, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Write.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Context-Type", "text/html")
+	c.Status(code)
+	c.Write.Write([]byte(html))
+}
