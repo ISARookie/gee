2bd76b8e160de930447cb7b50fd8b779057000a3
chenxu <chenxu@shanhai.team>
2023-07-18 14:28:04 +0800

    test:     http-context

--- gee-web/day2-context/gee/context.go
+++ gee-web/day2-context/gee/context.go
@@ -1,10 +1,66 @@
 package gee
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // H /** 自定义类型H 为string-interface map
 type H map[string]interface{}
 
 type Context struct {
-	Write http.ResponseWriter
+	Write      http.ResponseWriter
+	Req        *http.Request
+	Path       string
+	Method     string
+	StatusCode int
 }
+
+func newContext(w http.ResponseWriter, r *http.Request) *Context {
+	return &Context{
+		Write: w, Req: r, Path: r.URL.Path, Method: r.Method,
+	}
+}
+
+//	func (c *Context) PostForm(key string) string {
+//		return c.Req.FormValue(key)
+//	}
+//
+//	func (c *Context) Query(key string) string {
+//		return c.Req.URL.Query().Get(key)
+//	}
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Write.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Write.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Write.Write([]byte(fmt.Sprintf(format, values)))
+}
+
+//// JSON /** obj的作用？
+//func (c *Context) JSON(code int, obj interface{}) {
+//	c.SetHeader("Content-Type", "application/json")
+//	c.Status(code)
+//	encoder := json.NewEncoder(c.Write)
+//	if err := encoder.Encode(obj); err != nil {
+//		http.Error(c.Write, err.Error(), 500)
+//	}
+//}
+//
+//func (c *Context) Data(code int, data []byte) {
+//	c.Status(code)
+//	c.Write.Write(data)
+//}
+//
+//func (c *Context) HTML(code int, html string) {
+//	c.SetHeader("Context-Type", "text/html")
+//	c.Status(code)
+//	c.Write.Write([]byte(html))
+//}
